ef2ff135d33a1eb6fe2ed7215dc066a6c04853d9
Dias Junior <reinaldo.diasjunior@gmail.com>
2021-07-09 14:26:39 -0300

    feat(infra): add grpc server

---
+++ codebank/infrastructure/server/server.go
@@ -0,0 +1,35 @@
+package server
+
+import (
+	"log"
+	"net"
+
+	"github.com/diasjuniorr/code-commerce/codebank/infrastructure/grpc/pb"
+	"github.com/diasjuniorr/code-commerce/codebank/infrastructure/service"
+	"github.com/diasjuniorr/code-commerce/codebank/usecase"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type GRPCServer struct {
+	ProcessTransactionUseCase usecase.UseCaseTransaction
+}
+
+func NewGRPCServer() GRPCServer {
+	return GRPCServer{}
+}
+
+func (g *GRPCServer) Serve() {
+	lis, err := net.Listen("tcp", "0.0.0.0:50052")
+	if err != nil {
+		log.Fatal(err)
+	}
+	transactionService := service.NewTransactionService()
+	transactionService.ProcessTransactionUseCase = g.ProcessTransactionUseCase
+
+	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
+
+	pb.RegisterPaymentServiceServer(grpcServer, transactionService)
+	grpcServer.Serve(lis)
+}
