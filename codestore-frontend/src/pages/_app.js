0047cafea0a21f25d51f76d851d2069f3e4a1f7e
Dias Junior <reinaldo.diasjunior@gmail.com>
2021-08-16 13:04:11 -0300

    chore(app): add global theme and improve css

--- codestore-frontend/src/pages/_app.js
+++ codestore-frontend/src/pages/_app.js
@@ -1,7 +1,19 @@
 import '../../styles/globals.css'
+import {MuiThemeProvider, CssBaseline, Container, Box} from "@material-ui/core"
+import theme from "../theme"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <MuiThemeProvider theme={theme}>
+    <CssBaseline/>
+    <Container>
+      <Box marginTop={1}>
+        <Component {...pageProps} />
+      </Box>
+    </Container>
+  </MuiThemeProvider>
+  )
+  
 }
 
 export default MyApp
