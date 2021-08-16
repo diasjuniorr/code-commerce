d872d3107546df37c71928b60bf446c8445c3c71
Dias Junior <reinaldo.diasjunior@gmail.com>
2021-08-16 13:39:21 -0300

    feat(navbar): add navbar to app

--- codestore-frontend/src/pages/_app.js
+++ codestore-frontend/src/pages/_app.js
@@ -1,11 +1,13 @@
 import '../../styles/globals.css'
 import {MuiThemeProvider, CssBaseline, Container, Box} from "@material-ui/core"
+import Navbar from "../components/Navbar.tsx"
 import theme from "../theme"
 
 function MyApp({ Component, pageProps }) {
   return (
   <MuiThemeProvider theme={theme}>
     <CssBaseline/>
+    <Navbar/>
     <Container>
       <Box marginTop={1}>
         <Component {...pageProps} />
